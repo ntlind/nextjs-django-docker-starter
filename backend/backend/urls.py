cc2643ed6f26b519b81e408fcf70d57002dc1daa
Nick <nthorlind@gmail.com>
2022-01-23 11:32:04 -0700

    update REST to not worry about trailing slashes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,14 +6,22 @@ from django.contrib import admin
 
 
 # Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
+class OptionalSlashRouter(routers.SimpleRouter):
+    """Allow the url to have a trailing slash (or not)"""
+
+    def __init__(self):
+        super().__init__()
+        self.trailing_slash = "/?"
+
+
+router = OptionalSlashRouter()
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("", include(router.urls)),
     path("char_count/", views.CharCount.as_view(), name="char_count"),
-    path(r"^char_count$", views.CharCount.as_view(), name="char_count"),
+    path("char_count", views.CharCount.as_view(), name="char_count"),
     path("/", include("rest_framework.urls", namespace="rest_framework")),
     path(r"admin/", admin.site.urls),
 ]
