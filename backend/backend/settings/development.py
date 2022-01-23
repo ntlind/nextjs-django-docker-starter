193087acb84301496e703675f1ee2f0fc4fe2903
Nick <nthorlind@gmail.com>
2022-01-22 20:03:32 -0700

    start with working drf tutorial

--- backend/backend/settings/development.py
+++ backend/backend/settings/development.py
@@ -1,12 +1,12 @@
 from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cx8&=ho5h7_(0i_g&(j%@^0)3)(*lw7xq^2a2&v=4sder2gwaj'
+SECRET_KEY = 'ioasdhjojasdiofjo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django"]
+ALLOWED_HOSTS = ["django", "localhost"]
 
 INSTALLED_APPS += [
     'django_extensions',
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,25 @@
-from contrib.views import char_count
-from django.contrib import admin
-from django.urls import path, re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("char_count", char_count, name="char_count"),
-    re_path(".*", TemplateView.as_view(template_name="index.html")),
+    path('', include(router.urls)),
+    path('/', include('rest_framework.urls', namespace='rest_framework'))
 ]
